b585599df827c1879d5a06042befc62883948bdc
Gonzalo Sabatino <gsabatino@aeroterra.com>
2023-12-29 16:27:42 -0300

    agrego nombre gonzalo

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("hola como estas 2")
-print("me olvide de algo")
+import os
+
+def leer_contenido():
+    archivos = os.listdir("./carpeta")
+
+    for archivo in archivos:
+        with open("./carpeta/"+archivo, "r") as f:
+            print(f.readlines())
+
+leer_contenido()
